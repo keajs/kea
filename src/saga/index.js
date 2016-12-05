74d5bdbd68c02c1064c0275726ee452d4c99f0a3
Marius Andra <marius.andra@gmail.com>
2016-12-05 19:25:07 +0100

    bind it all at the start

--- src/saga/index.js
+++ src/saga/index.js
@@ -4,14 +4,32 @@ import { call, cancelled } from 'redux-saga/effects'
 import { selectActionsFromLogic } from '../logic'
 
 class Saga {
+  constructor () {
+    this._saga = null
+  }
+
   init () {
+    if (this._saga) {
+      return this._saga
+    }
+
+    // bind all functions to this
+    const keys = Object.keys(this)
+    for (let i = 0; i < keys.length; i++) {
+      if (typeof this[keys[i]] === 'function') {
+        this[keys[i]] = this[keys[i]].bind(this)
+      }
+    }
+
+    // run and assign actions
     let object = {}
     object.actions = this.actions ? selectActionsFromLogic(this.actions(object)) : {}
     Object.assign(this, object)
 
     const _this = this
 
-    return function * () {
+    // generate the saga
+    this._saga = function * () {
       try {
         // run takeEvery and takeLatest
         let ops = { takeEvery, takeLatest }
@@ -26,10 +44,10 @@ class Saga {
               let fn = list[keys[i]]
               if (Array.isArray(fn)) {
                 for (let j = 0; j < fn.length; j++) {
-                  yield ops[op](keys[i], fn[j].bind(_this))
+                  yield ops[op](keys[i], fn[j])
                 }
               } else {
-                yield ops[op](keys[i], fn.bind(_this))
+                yield ops[op](keys[i], fn)
               }
             }
           }
@@ -37,17 +55,19 @@ class Saga {
 
         // call the run function
         if (_this.run) {
-          yield call(_this.run.bind(_this))
+          yield call(_this.run)
         }
       } finally {
         // call the cancelled function if cancelled
         if (yield cancelled()) {
           if (_this.cancelled) {
-            yield call(_this.cancelled.bind(_this))
+            yield call(_this.cancelled)
           }
         }
       }
     }
+
+    return this._saga
   }
 }
 
