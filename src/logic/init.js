d31901e5a0b5fe35f2a6f6cde68e3c2a08df7b25
Marius Andra <marius.andra@gmail.com>
2017-07-04 08:17:04 +0200

    refactor init

---
+++ src/logic/init.js
@@ -0,0 +1,95 @@
+import { select } from 'redux-saga/effects'
+import { createSelector } from 'reselect'
+
+import { combineReducerObjects, convertReducerArrays } from './reducer'
+import { pathSelector, createSelectors } from './selectors'
+import { createActions } from './actions'
+
+let gaveAddSelectorWarning = false
+let gaveStructureWarning = false
+
+// convert ['A', 'B'] ==> { 'A': 'A', 'B': 'B' }
+function convertConstants (c) {
+  if (Array.isArray(c)) {
+    let a = {}
+    for (let i = 0; i < c.length; i++) {
+      a[c[i]] = c[i]
+    }
+    return a
+  }
+  return c
+}
+
+export function init (_this, object) {
+  object.path = _this.path()
+  object.selector = (state) => pathSelector(object.path, state)
+  object.constants = _this.constants ? convertConstants(_this.constants(object)) : {}
+  object.actions = _this.actions ? createActions(_this.actions(object), object.path) : {}
+
+  // reducers
+  if (_this.structure) {
+    // DEPRECATED
+    if (!gaveStructureWarning) {
+      console.warn(`[KEA-LOGIC] structure = () => ({}) is deprecated. Please rename it to reducers = () => ({}).`)
+      gaveStructureWarning = true
+    }
+    object.reducers = convertReducerArrays(_this.structure(object))
+  } else if (_this.reducers) {
+    object.reducers = convertReducerArrays(_this.reducers(object))
+  } else {
+    object.reducers = {}
+  }
+  object.reducer = _this.reducer ? _this.reducer(object) : combineReducerObjects(_this.path, _this.reducers)
+
+  object.selectors = createSelectors(object.path, Object.keys(object.reducers))
+
+  // selectors
+  // TODO: remove addSelector deprecation
+  let response = _this.selectors({...object, addSelector: _addSelector.bind(object)})
+
+  if (typeof response === 'object') {
+    const keys = Object.keys(response)
+    for (let i = 0; i < keys.length; i++) {
+      const s = response[keys[i]]
+      // s == [() => args, selectorFunction, propType]
+
+      const args = s[0]()
+      if (s[2]) {
+        object.reducers[keys[i]] = { type: s[2] }
+      }
+      object.selectors[keys[i]] = createSelector(...args, s[1])
+    }
+  }
+
+  Object.assign(_this, object)
+
+  _this.get = function * (key) {
+    return yield select(key ? _this.selectors[key] : _this.selector)
+  }
+
+  _this.fetch = function * () {
+    let results = {}
+
+    const keys = Array.isArray(arguments[0]) ? arguments[0] : arguments
+
+    for (let i = 0; i < keys.length; i++) {
+      results[keys[i]] = yield _this.get(keys[i])
+    }
+
+    return results
+  }
+
+  return _this
+}
+
+// DEPRECATED
+// bound to object
+function _addSelector (name, type, args, func) {
+  if (!gaveAddSelectorWarning) {
+    console.warn(`[KEA-LOGIC] addSelector is deprecated. Please use the new compact Array format.`)
+    gaveAddSelectorWarning = true
+  }
+
+  this.reducers[name] = { type }
+  this.selectors[name] = createSelector(...args, func)
+}
--- src/logic/logic.js
+++ src/logic/logic.js
@@ -1,24 +1,5 @@
-import { select } from 'redux-saga/effects'
-import { createSelector } from 'reselect'
-
-import { combineReducerObjects, convertReducerArrays } from './reducer'
-import { pathSelector, createSelectors } from './selectors'
-import { createActions } from './actions'
-
-let gaveAddSelectorWarning = false
-let gaveStructureWarning = false
-
-// convert ['A', 'B'] ==> { 'A': 'A', 'B': 'B' }
-function convertConstants (c) {
-  if (Array.isArray(c)) {
-    let a = {}
-    for (let i = 0; i < c.length; i++) {
-      a[c[i]] = c[i]
-    }
-    return a
-  }
-  return c
-}
+import { init } from './init'
+import { combineReducerObjects } from './reducer'
 
 export function initLogic (Klass) {
   return new Klass().init()
@@ -35,75 +16,7 @@ class Logic {
 
   init () {
     let object = {}
-
-    object.path = this.path()
-    object.selector = (state) => pathSelector(object.path, state)
-    object.constants = convertConstants(this.constants(object))
-    object.actions = createActions(this.actions(object), object.path)
-
-    // reducers
-    if (this.structure) {
-      // DEPRECATED
-      if (!gaveStructureWarning) {
-        console.warn(`[KEA-LOGIC] structure = () => ({}) is deprecated. Please rename it to reducers = () => ({}).`)
-        gaveStructureWarning = true
-      }
-      object.reducers = convertReducerArrays(this.structure(object))
-    } else {
-      object.reducers = convertReducerArrays(this.reducers(object))
-    }
-    object.reducer = this.reducer(object)
-
-    object.selectors = createSelectors(object.path, Object.keys(object.reducers))
-
-    // selectors
-    // TODO: remove addSelector deprecation
-    let response = this.selectors({...object, addSelector: object::this._addSelector})
-
-    if (typeof response === 'object') {
-      const keys = Object.keys(response)
-      for (let i = 0; i < keys.length; i++) {
-        const s = response[keys[i]]
-        // s == [() => args, selectorFunction, propType]
-
-        const args = s[0]()
-        if (s[2]) {
-          object.reducers[keys[i]] = { type: s[2] }
-        }
-        object.selectors[keys[i]] = createSelector(...args, s[1])
-      }
-    }
-
-    Object.assign(this, object)
-
-    return this
-  }
-
-  // DEPRECATED
-  _addSelector (name, type, args, func) {
-    if (!gaveAddSelectorWarning) {
-      console.warn(`[KEA-LOGIC] addSelector is deprecated. Please use the new compact Array format.`)
-      gaveAddSelectorWarning = true
-    }
-
-    this.reducers[name] = { type }
-    this.selectors[name] = createSelector(...args, func)
-  }
-
-  * get (key) {
-    return yield select(key ? this.selectors[key] : this.selector)
-  }
-
-  * fetch () {
-    let results = {}
-
-    const keys = Array.isArray(arguments[0]) ? arguments[0] : arguments
-
-    for (let i = 0; i < keys.length; i++) {
-      results[keys[i]] = yield this.get(keys[i])
-    }
-
-    return results
+    return init(this, object)
   }
 }
 
