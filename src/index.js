a8231d02e5e64ac2b9b9a1e5269aed02b480907c
Marius Andra <marius.andra@gmail.com>
2017-07-07 09:22:34 +0200

    @connect delegates to @kea

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-export { kea, connect, createLogic } from './logic'
+import { kea } from './logic'
+export { kea, createLogic } from './logic'
 export { createSaga } from './saga'
 export { keaReducer, keaSaga } from './scene'
+
+export const connect = (mapping) => kea({ connect: mapping })
--- src/logic/kea.js
+++ src/logic/kea.js
@@ -23,58 +23,73 @@ const DEBUG = false
 
 export function kea (_this) {
   return function (Klass) {
-    // createLogic(_this, )
+    const hasMapping = !!(_this.connect)
+    const hasLogic = !!(_this.actions || _this.reducer || _this.selectors)
+    const hasSaga = !!(_this.sagas || _this.start || _this.stop || _this.takeEvery || _this.takeLatest)
+
     let object = {}
+    let propTypes = {}
+    let mapping = {}
+    let connectedActions = {}
+    let connectedSelectors = {}
 
     // pregenerate as many things as we can
-    object.path = _this.path('').filter(p => p)
-    object.constants = _this.constants ? convertConstants(_this.constants(object)) : {}
-    object.actions = _this.actions ? createActions(_this.actions(object), object.path) : {}
-    object.props = {}
-
-    // the { connect: { props, actions } } part
-    const mapping = _this.connect || {}
-
-    // get default proptypes and add connected ones
-    let propTypes = Object.assign({}, mapping.props ? propTypesFromMapping(mapping) : {}, Klass.propTypes || {})
-
-    // add proptypes from reducer
-    const reducers = _this.reducers ? convertReducerArrays(_this.reducers(object)) : {}
-    Object.keys(reducers).forEach(reducerKey => {
-      if (reducers[reducerKey].type) {
-        propTypes[reducerKey] = reducers[reducerKey].type
-      }
-    })
-
-    // add proptypes from selectors
-    const selectorsThatDontWork = _this.selectors ? _this.selectors({}) : {}
-    Object.keys(selectorsThatDontWork).forEach(selectorKey => {
-      if (selectorsThatDontWork[selectorKey][2]) {
-        propTypes[selectorKey] = selectorsThatDontWork[selectorKey][2]
-      }
-    })
-
-    // add kea metadata to component
-    Klass.kea = {
-      path: _this.path,
-      constants: object.constants,
-      actions: object.actions,
-      find: (key) => cachedSelectors(_this.path(key))
+    if (hasLogic) {
+      object.path = _this.path('').filter(p => p)
+      object.constants = _this.constants ? convertConstants(_this.constants(object)) : {}
+      object.actions = _this.actions ? createActions(_this.actions(object), object.path) : {}
+      object.props = {}
     }
 
-    // convert this.props.actions to this.actions in the component
-    const originalComponentWillMount = Klass.prototype.componentWillMount
-    Klass.prototype.componentWillMount = function () {
-      this.actions = this.props.actions
-      originalComponentWillMount && originalComponentWillMount.bind(this)()
+    if (hasMapping) {
+      // the { connect: { props, actions } } part
+      mapping = _this.connect || {}
+
+      // get default proptypes and add connected ones
+      propTypes = Object.assign({}, mapping.props ? propTypesFromMapping(mapping) : {}, Klass.propTypes || {})
+
+      // connected actions and props/selectors
+      connectedActions = createActionTransforms(mapping.actions).actions
+      connectedSelectors = createPropTransforms(mapping.props).selectorFunctions
     }
 
-    // connected actions and props/selectors
-    const connectedActions = createActionTransforms(mapping.actions).actions
-    const connectedSelectors = createPropTransforms(mapping.props).selectorFunctions
+    if (hasLogic) {
+      // add proptypes from reducer
+      const reducers = _this.reducers ? convertReducerArrays(_this.reducers(object)) : {}
+      Object.keys(reducers).forEach(reducerKey => {
+        if (reducers[reducerKey].type) {
+          propTypes[reducerKey] = reducers[reducerKey].type
+        }
+      })
+
+      // add proptypes from selectors
+      const selectorsThatDontWork = _this.selectors ? _this.selectors({}) : {}
+      Object.keys(selectorsThatDontWork).forEach(selectorKey => {
+        if (selectorsThatDontWork[selectorKey][2]) {
+          propTypes[selectorKey] = selectorsThatDontWork[selectorKey][2]
+        }
+      })
+
+      // add kea metadata to component
+      Klass.kea = {
+        path: _this.path,
+        constants: object.constants,
+        actions: object.actions,
+        find: (key) => cachedSelectors(_this.path(key))
+      }
+    }
+
+    if (hasLogic || hasMapping) {
+      // convert this.props.actions to this.actions in the component
+      const originalComponentWillMount = Klass.prototype.componentWillMount
+      Klass.prototype.componentWillMount = function () {
+        this.actions = this.props.actions
+        originalComponentWillMount && originalComponentWillMount.bind(this)()
+      }
+    }
 
     // check for sagas
-    if (_this.sagas || _this.start || _this.stop || _this.takeEvery || _this.takeLatest) {
+    if (hasSaga) {
       let runningSaga
 
       const originalComponentDidMount = Klass.prototype.componentDidMount
@@ -84,10 +99,9 @@ export function kea (_this) {
         }
 
         // this === component instance
-
         this._sagaBase = {}
 
-        const key = _this.key(this.props)
+        const key = _this.key ? _this.key(this.props) : 'index'
         const path = _this.path(key)
 
         let sagas = _this.sagas || []
@@ -174,135 +188,151 @@ export function kea (_this) {
       let result = null
 
       return (nextState, nextOwnProps) => {
-        const key = _this.key ? _this.key(nextOwnProps) : 'index'
-
-        if (typeof key === 'undefined') {
-          console.error(`"key" can't be undefined in path: ${_this.path('undefined').join('.')}`)
-        }
-
-        const path = _this.path(key)
-        const joinedPath = path.join('.')
-
-        if (DEBUG) {
-          console.log(`Inline selectorFactory for ${joinedPath}`)
-          console.log({ nextOwnProps, nextState, key, path })
-        }
-
-        let selector
-        let selectors
-
-        // now we must check if the reducer is already in redux, or we need to add it
-        // if we need to add it, create "dummy" selectors for the default values until then
-
-        // is the reducer created? if we have "true" in the cache, it's definitely created
-        let reducerCreated = inlineCache[joinedPath] && inlineCache[joinedPath].reducerCreated
-
-        // if it's not let's double check. maybe it is now?
-        if (!reducerCreated) {
-          try {
-            selector = (state) => pathSelector(path, state)
-            reducerCreated = typeof selector(nextState) !== 'undefined'
-          } catch (e) {
-            reducerCreated = false
-          }
-        }
-
-        // we have the selectors cached! with the current reducerCreated state!
-        if (inlineCache[joinedPath] && inlineCache[joinedPath].reducerCreated === reducerCreated) {
-          if (DEBUG) {
-            console.log('cache hit!')
-          }
-          selector = inlineCache[joinedPath].selector
-          selectors = inlineCache[joinedPath].selectors
-
-        // either we have nothing cached or the cache is invalid. regenerate the selectors!
-        } else {
-          if (!selector) {
-            selector = (state) => pathSelector(path, state)
-          }
-
-          // add { path } and { key } to the reducer creator function
-          let localObject = Object.assign({}, object, { path, key, props: nextOwnProps })
-          localObject.reducers = _this.reducers ? convertReducerArrays(_this.reducers(localObject)) : {}
-          localObject.reducer = _this.reducer ? _this.reducer(localObject) : combineReducerObjects(path, localObject.reducers)
-
-          // if the reducer is in redux, get real reducer selectors. otherwise add dummies that return defaults
-          if (reducerCreated) {
-            selectors = createSelectors(path, Object.keys(localObject.reducers))
-          } else {
-            addReducer(path, localObject.reducer, true)
-            selectors = {}
-            Object.keys(localObject.reducers).forEach(key => {
-              selectors[key] = () => localObject.reducers[key].value
-            })
-          }
-
-          // create
-          const selectorResponse = _this.selectors ? _this.selectors(Object.assign({}, localObject, { selectors })) : {}
-
-          Object.keys(selectorResponse).forEach(selectorKey => {
-            // s == [() => args, selectorFunction, propType]
-            const s = selectorResponse[selectorKey]
-
-            const args = s[0]()
-            selectors[selectorKey] = createSelector(...args, s[1])
-          })
-
-          // store in the cache
-          inlineCache[joinedPath] = {
-            reducerCreated,
-            selector,
-            selectors
-          }
-        }
-
-        if (DEBUG) {
-          console.log({ selector, selectors })
-        }
+        let key
+        let path
+        let joinedPath
 
         let nextProps = Object.assign({}, nextOwnProps)
 
-        // connected props
-        Object.keys(connectedSelectors).forEach(propKey => {
-          nextProps[propKey] = connectedSelectors[propKey](nextState)
-        })
+        if (hasMapping) {
+          // connected props
+          Object.keys(connectedSelectors).forEach(propKey => {
+            nextProps[propKey] = connectedSelectors[propKey](nextState)
+          })
+        }
 
-        Object.keys(selectors).forEach(selectorKey => {
-          nextProps[selectorKey] = selectors[selectorKey](nextState)
-        })
+        if (hasLogic) {
+          key = _this.key ? _this.key(nextOwnProps) : 'index'
+
+          if (typeof key === 'undefined') {
+            console.error(`"key" can't be undefined in path: ${_this.path('undefined').join('.')}`)
+          }
+
+          path = _this.path(key)
+          joinedPath = path.join('.')
+
+          if (DEBUG) {
+            console.log(`Inline selectorFactory for ${joinedPath}`)
+            console.log({ nextOwnProps, nextState, key, path })
+          }
+
+          let selector
+          let selectors
+
+          // now we must check if the reducer is already in redux, or we need to add it
+          // if we need to add it, create "dummy" selectors for the default values until then
+
+          // is the reducer created? if we have "true" in the cache, it's definitely created
+          let reducerCreated = inlineCache[joinedPath] && inlineCache[joinedPath].reducerCreated
+
+          // if it's not let's double check. maybe it is now?
+          if (!reducerCreated) {
+            try {
+              selector = (state) => pathSelector(path, state)
+              reducerCreated = typeof selector(nextState) !== 'undefined'
+            } catch (e) {
+              reducerCreated = false
+            }
+          }
+
+          // we have the selectors cached! with the current reducerCreated state!
+          if (inlineCache[joinedPath] && inlineCache[joinedPath].reducerCreated === reducerCreated) {
+            if (DEBUG) {
+              console.log('cache hit!')
+            }
+            selector = inlineCache[joinedPath].selector
+            selectors = inlineCache[joinedPath].selectors
+
+          // either we have nothing cached or the cache is invalid. regenerate the selectors!
+          } else {
+            if (!selector) {
+              selector = (state) => pathSelector(path, state)
+            }
+
+            // add { path } and { key } to the reducer creator function
+            let localObject = Object.assign({}, object, { path, key, props: nextOwnProps })
+            localObject.reducers = _this.reducers ? convertReducerArrays(_this.reducers(localObject)) : {}
+            localObject.reducer = _this.reducer ? _this.reducer(localObject) : combineReducerObjects(path, localObject.reducers)
+
+            // if the reducer is in redux, get real reducer selectors. otherwise add dummies that return defaults
+            if (reducerCreated) {
+              selectors = createSelectors(path, Object.keys(localObject.reducers))
+            } else {
+              addReducer(path, localObject.reducer, true)
+              selectors = {}
+              Object.keys(localObject.reducers).forEach(key => {
+                selectors[key] = () => localObject.reducers[key].value
+              })
+            }
+
+            // create
+            const selectorResponse = _this.selectors ? _this.selectors(Object.assign({}, localObject, { selectors })) : {}
+
+            Object.keys(selectorResponse).forEach(selectorKey => {
+              // s == [() => args, selectorFunction, propType]
+              const s = selectorResponse[selectorKey]
+
+              const args = s[0]()
+              selectors[selectorKey] = createSelector(...args, s[1])
+            })
+
+            // store in the cache
+            inlineCache[joinedPath] = {
+              reducerCreated,
+              selector,
+              selectors
+            }
+          }
+
+          if (DEBUG) {
+            console.log({ selector, selectors })
+          }
+
+          Object.keys(selectors).forEach(selectorKey => {
+            nextProps[selectorKey] = selectors[selectorKey](nextState)
+          })
+        }
 
         if (DEBUG) {
           console.log({ nextProps })
         }
 
-        let actions = actionCache[joinedPath]
+        // TODO: cache these even if no path present
+        let actions = joinedPath ? actionCache[joinedPath] : null
 
         if (!actions) {
           actions = {}
 
-          // pass conneted actions as they are
-          Object.keys(connectedActions).forEach(actionKey => {
-            actions[actionKey] = (...args) => dispatch(connectedActions[actionKey](...args))
-          })
+          if (hasMapping) {
+            // pass conneted actions as they are
+            Object.keys(connectedActions).forEach(actionKey => {
+              actions[actionKey] = (...args) => dispatch(connectedActions[actionKey](...args))
+            })
+          }
 
-          // inject key to the payload of inline actions
-          Object.keys(object.actions).forEach(actionKey => {
-            actions[actionKey] = (...args) => {
-              const createdAction = object.actions[actionKey](...args)
-              return dispatch({
-                ...createdAction,
-                payload: {
-                  key,
-                  ...createdAction.payload
-                }
-              })
-            }
-          })
+          if (hasLogic) {
+            // inject key to the payload of inline actions
+            Object.keys(object.actions).forEach(actionKey => {
+              actions[actionKey] = (...args) => {
+                const createdAction = object.actions[actionKey](...args)
+                return dispatch({
+                  ...createdAction,
+                  payload: {
+                    key,
+                    ...createdAction.payload
+                  }
+                })
+              }
+            })
+          }
+
           if (DEBUG) {
             console.log({ actions })
           }
 
-          actionCache[joinedPath] = actions
+          if (joinedPath) {
+            actionCache[joinedPath] = actions
+          }
         }
 
         // if the props did not change, return the old cached object
