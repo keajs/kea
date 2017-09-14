2c88fe2cfe7cf6f9c1588cbac5b5edd765a0a0be
Marius Andra <marius.andra@gmail.com>
2017-09-14 11:56:52 +0200

    remove deprecated logic and scene files

--- src/scene.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./scene/index.js')
--- src/scene/index.js
+++
@@ -1,6 +0,0 @@
-// logic
-import createCombinedSaga from '../kea/saga/create-combined'
-export { createCombinedSaga }
-export { keaReducer, addReducer } from '../kea/reducer'
-
-export { NEW_SCENE, Scene, createScene, addKeaScene, getRoutes, combineScenesAndRoutes, createRootSaga } from './_deprecated'
