e9ea51036a463452a5c16db136c508904375b9a6
Marius Andra <marius.andra@gmail.com>
2017-09-27 07:47:47 +0200

    remove logic.js entrypoint

--- src/logic/index.js
+++
@@ -1,8 +0,0 @@
-// logic
-export { kea } from './kea'
-export { pathSelector, safePathSelector, createSelectors } from './selectors'
-export { createAction, createActions, selectActionsFromLogic } from './actions'
-export { createReducer, createPersistentReducer, combineReducerObjects } from './reducer'
-
-// component
-export { selectPropsFromLogic, propTypesFromMapping, havePropsChanged } from './props'
