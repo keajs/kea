9f6ac1a46198e8ce5b4166f951564a7e59f952a1
Marius Andra <marius.andra@gmail.com>
2019-04-18 16:29:57 +0200

    add this.actions support to react components

--- src/kea/index.js
+++ src/kea/index.js
@@ -3,10 +3,14 @@ import { connect } from 'react-redux'
 import { convertInputToLogic, convertPartialDynamicInput, clearLogicCache } from '../logic/index'
 
 export function kea (input) {
-  const wrapper = connect(
-    mapStateToPropsCreator(input),
-    mapDispatchToPropsCreator(input)
-  )
+  const wrapper = (Klass) => {
+    injectActionsIntoClass(Klass)
+
+    return connect(
+      mapStateToPropsCreator(input),
+      mapDispatchToPropsCreator(input)
+    )(Klass)
+  }
 
   wrapper._isKeaFunction = true
   wrapper._isKeaSingleton = !input.key
@@ -43,6 +47,27 @@ const mapDispatchToPropsCreator = input => (dispatch, ownProps) => {
   return {
     dispatch: dispatch,
     actions: actions
+  }
+}
+
+function isStateless (Component) {
+  return (
+    typeof Component === 'function' && // can be various things
+    !(Component.prototype && Component.prototype.isReactComponent) // native arrows don't have prototypes // special property
+  )
+}
+
+function injectActionsIntoClass (Klass) {
+  if (!isStateless(Klass)) {
+    // inject to the component something that
+    // converts this.props.actions to this.actions
+    if (!Object.getOwnPropertyDescriptor(Klass.prototype, 'actions')) {
+      Object.defineProperty(Klass.prototype, 'actions', {
+        get: function actions () {
+          return this.props.actions
+        }
+      })
+    }
   }
 }
 
