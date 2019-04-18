739bb4aa11f9cfffe327d59a410072faec108f81
Marius Andra <marius.andra@gmail.com>
2019-04-18 22:32:24 +0200

    update beta todo list

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@
 // 3+ have something easy to hook up to redux
 // 4. typescript support?
 // 5. easy to use plugins
-// 6. connect working with prettier
-// 7- could precalculate data?
+// 6. connect giving nicer output with prettier
+// 7- could precalculate data, e.g. with prepack?
 // 8+ exactly the same code for withkey or without
 // 9+ track mounting and unmounting of logic stores
+// 0. set proptypes on the component
 
 import { resetKeaLogicCache } from './kea'
 import { clearReducerCache } from './store'
