7a6b0e0d551034d1b78ca7578f886eb3d1a3ad34
Marius Andra <marius.andra@gmail.com>
2019-04-24 16:28:03 +0200

    export action type creator

--- src/logic/actions.js
+++ src/logic/actions.js
@@ -4,6 +4,12 @@ let actionCache = {}
 const isObject = (item) => typeof item === 'object' && !Array.isArray(item) && item !== null
 const toSpaces = (key) => key.replace(/(?:^|\.?)([A-Z])/g, (x, y) => ' ' + y.toLowerCase()).replace(/^ /, '')
 
+export function createActionType (key, path) {
+  // remove 'scenes.' from the path
+  const pathString = (path[0] === 'scenes' ? path.slice(1) : path).join('.')
+  return `${toSpaces(key)} (${pathString})`
+}
+
 export function createAction (type, payloadCreator) {
   if (actionCache[type]) {
     return actionCache[type]
@@ -32,11 +38,7 @@ export function createActions (input, output) {
   const path = output.path
   const payloadCreators = input.actions(input)
 
-  // remove scenes from path
-  const pathString = (path[0] === 'scenes' ? path.slice(1) : path).join('.')
-
   Object.keys(payloadCreators).forEach(key => {
-    const type = `${toSpaces(key)} (${pathString})`
-    output.actions[key] = createAction(type, payloadCreators[key])
+    output.actions[key] = createAction(createActionType(key, path), payloadCreators[key])
   })
 }
