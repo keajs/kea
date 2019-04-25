a03fb7afdbaca2e4c31ec40d8ac60b3d05756a6b
Marius Andra <marius.andra@gmail.com>
2019-04-25 09:55:33 +0200

    1.0 last steps

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
-// TODO:
-// 1+ convert kea input structure to actions, reducers and selectors
-// 2+ skip the step of doing it twice, both before and after connection
-// 3+ have something easy to hook up to redux
-// 4. typescript support?
-// 5+ easy to use plugins
-// 6. connect giving nicer output with prettier
-// 7- could precalculate data, e.g. with prepack?
-// 8+ exactly the same code for withkey or without
-// 9+ track mounting and unmounting of logic stores
-// 0. set proptypes on the component
-
 // 1.0 todo
-// + set proptypes on the component
-// + .withKey
 // . merge logic
 // . lazy mode cleanup
 // . listeners plugin
