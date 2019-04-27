54c4057cd2f11dbef039ad8e855905693b8de101
Marius Andra <marius.andra@gmail.com>
2019-04-27 13:14:27 +0200

    test lazy logic detachment

--- src/__tests__/detach-reducer.js
+++ src/__tests__/detach-reducer.js
@@ -27,3 +27,158 @@ test('detaches from a simple state', () => {
   expect(dispatched.length).toEqual(1)
   expect(dispatched[0].type).toEqual(DETACH_REDUCER)
 })
+
+test('detaches from a state with 2 reducers', () => {
+  const reducer1 = () => {}
+  const reducer2 = () => {}
+
+  getCache().reducerTree = {
+    scenes: { something: { 1: reducer1, 2: reducer2 } }
+  }
+
+  let dispatched = []
+
+  getCache().store = {
+    dispatch: (action) => dispatched.push(action)
+  }
+
+  detachReducer(['scenes', 'something', 1])
+
+  expect(getCache().reducerTree).toEqual({ scenes: { something: { 2: reducer2 } } })
+  expect(dispatched.length).toEqual(1)
+  expect(dispatched[0].type).toEqual(DETACH_REDUCER)
+
+  detachReducer(['scenes', 'something', 2])
+
+  expect(getCache().reducerTree).toEqual({ scenes: {} })
+  expect(dispatched.length).toEqual(2)
+  expect(dispatched[1].type).toEqual(DETACH_REDUCER)
+})
+
+test('detaches from a state with 2 nested reducers, part 1', () => {
+  const reducer1 = () => {}
+  const reducer2 = () => {}
+
+  getCache().reducerTree = {
+    scenes: { something: { 1: reducer1, bla: { 2: reducer2 } } }
+  }
+
+  let dispatched = []
+
+  getCache().store = {
+    dispatch: (action) => dispatched.push(action)
+  }
+
+  detachReducer(['scenes', 'something', 'bla', 2])
+
+  expect(getCache().reducerTree).toEqual({ scenes: { something: { 1: reducer1 } } })
+  expect(dispatched.length).toEqual(1)
+  expect(dispatched[0].type).toEqual(DETACH_REDUCER)
+
+  detachReducer(['scenes', 'something', 1])
+
+  expect(getCache().reducerTree).toEqual({ scenes: {} })
+  expect(dispatched.length).toEqual(2)
+  expect(dispatched[1].type).toEqual(DETACH_REDUCER)
+})
+
+test('detaches from a state with 2 nested reducers, part 1', () => {
+  const reducer1 = () => {}
+  const reducer2 = () => {}
+
+  getCache().reducerTree = {
+    scenes: { something: { cat: { 1: reducer1 }, bla: { 2: reducer2 } } }
+  }
+
+  let dispatched = []
+
+  getCache().store = {
+    dispatch: (action) => dispatched.push(action)
+  }
+
+  detachReducer(['scenes', 'something', 'bla', 2])
+
+  expect(getCache().reducerTree).toEqual({ scenes: { something: { cat: { 1: reducer1 } } } })
+  expect(dispatched.length).toEqual(1)
+  expect(dispatched[0].type).toEqual(DETACH_REDUCER)
+
+  detachReducer(['scenes', 'something', 'cat', 1])
+
+  expect(getCache().reducerTree).toEqual({ scenes: {} })
+  expect(dispatched.length).toEqual(2)
+  expect(dispatched[1].type).toEqual(DETACH_REDUCER)
+})
+
+test('detaches from a state with a low reducer', () => {
+  const reducer1 = () => {}
+
+  getCache().reducerTree = {
+    scenes: { something: reducer1 }
+  }
+
+  let dispatched = []
+
+  getCache().store = {
+    dispatch: (action) => dispatched.push(action)
+  }
+
+  detachReducer(['scenes', 'something'])
+
+  expect(getCache().reducerTree).toEqual({ scenes: {} })
+  expect(dispatched.length).toEqual(1)
+  expect(dispatched[0].type).toEqual(DETACH_REDUCER)
+})
+
+test('cleans up a nested tree, vol 1', () => {
+  const reducer1 = () => {}
+  const reducer2 = () => {}
+
+  getCache().reducerTree = {
+    scenes: { something: { cat: { foo: { bar: { 1: reducer1 } } }, bla: { moo: { 2: reducer2 } } } }
+  }
+
+  let dispatched = []
+
+  getCache().store = {
+    dispatch: (action) => dispatched.push(action)
+  }
+
+  detachReducer(['scenes', 'something', 'bla', 'moo', 2])
+
+  expect(getCache().reducerTree).toEqual({ scenes: { something: { cat: { foo: { bar: { 1: reducer1 } } } } } })
+  expect(dispatched.length).toEqual(1)
+  expect(dispatched[0].type).toEqual(DETACH_REDUCER)
+
+  detachReducer(['scenes', 'something', 'cat', 'foo', 'bar', 1])
+
+  expect(getCache().reducerTree).toEqual({ scenes: {} })
+  expect(dispatched.length).toEqual(2)
+  expect(dispatched[1].type).toEqual(DETACH_REDUCER)
+})
+
+test('cleans up a nested tree, vol 2', () => {
+  const reducer1 = () => {}
+  const reducer2 = () => {}
+
+  getCache().reducerTree = {
+    scenes: { something: { cat: { foo: { bar: { 1: reducer1 } } }, bla: { moo: { 2: reducer2 } } } }
+  }
+
+  let dispatched = []
+
+  getCache().store = {
+    dispatch: (action) => dispatched.push(action)
+  }
+
+  detachReducer(['scenes', 'something', 'cat', 'foo', 'bar', 1])
+
+  expect(getCache().reducerTree).toEqual({ scenes: { something: { bla: { moo: { 2: reducer2 } } } } })
+  expect(dispatched.length).toEqual(1)
+  expect(dispatched[0].type).toEqual(DETACH_REDUCER)
+
+  detachReducer(['scenes', 'something', 'bla', 'moo', 2])
+
+  expect(getCache().reducerTree).toEqual({ scenes: {} })
+  expect(dispatched.length).toEqual(2)
+  expect(dispatched[1].type).toEqual(DETACH_REDUCER)
+})
---
+++ src/__tests__/lazy-connect.js
@@ -0,0 +1,112 @@
+/* global test, expect, beforeEach */
+import { kea, getStore, resetKeaCache } from '../index'
+
+import './helper/jsdom'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { mount, configure } from 'enzyme'
+import { Provider } from 'react-redux'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
+
+beforeEach(() => {
+  resetKeaCache()
+})
+
+test('connected lazy logic loading builds it', () => {
+  const store = getStore()
+
+  const connectedLogic = kea({
+    options: { lazy: true },
+
+    actions: () => ({
+      updateDescription: description => ({ description })
+    }),
+
+    reducers: ({ actions }) => ({
+      description: ['default', PropTypes.string, {
+        [actions.updateDescription]: (_, payload) => payload.description
+      }]
+    })
+  })
+
+  const logic = kea({
+    options: { lazy: true },
+
+    connect: {
+      props: [connectedLogic, ['description']]
+    },
+
+    actions: () => ({
+      updateName: name => ({ name })
+    }),
+
+    reducers: ({ actions }) => ({
+      name: ['chirpy', PropTypes.string, {
+        [actions.updateName]: (state, payload) => payload.name
+      }]
+    }),
+
+    selectors: ({ selectors }) => ({
+      capitalizedName: [
+        () => [selectors.name],
+        (name) => {
+          return name.trim().split(' ').map(k => `${k.charAt(0).toUpperCase()}${k.slice(1).toLowerCase()}`).join(' ')
+        },
+        PropTypes.string
+      ]
+    })
+  })
+
+  // nothing yet in the store
+  expect(store.getState()).toEqual({ kea: {}, scenes: {} })
+
+  const SampleComponent = ({ id, name, capitalizedName, description, actions: { updateName } }) => (
+    <div>
+      <div className='id'>{id}</div>
+      <div className='name'>{name}</div>
+      <div className='capitalizedName'>{capitalizedName}</div>
+      <div className='description'>{description}</div>
+      <div className='updateName' onClick={updateName}>updateName</div>
+    </div>
+  )
+
+  expect(store.getState()).toEqual({ kea: {}, scenes: {} })
+
+  const ConnectedComponent = logic(SampleComponent)
+
+  expect(store.getState()).toEqual({ kea: {}, scenes: {} })
+
+  const wrapper = mount(
+    <Provider store={store}>
+      <ConnectedComponent id={12} />
+    </Provider>
+  )
+
+  expect(store.getState()).toEqual({ kea: { inline: { 1: { name: 'chirpy' }, 2: { description: 'default' } } }, scenes: {} })
+
+  expect(wrapper.find('.id').text()).toEqual('12')
+  expect(wrapper.find('.name').text()).toEqual('chirpy')
+  expect(wrapper.find('.capitalizedName').text()).toEqual('Chirpy')
+  expect(wrapper.find('.description').text()).toEqual('default')
+
+  expect(store.getState()).toEqual({ kea: { inline: { 1: { name: 'chirpy' }, 2: { description: 'default' } } }, scenes: {} })
+
+  store.dispatch(logic.actions.updateName('somename'))
+  store.dispatch(connectedLogic.actions.updateDescription('new description'))
+
+  expect(store.getState()).toEqual({ kea: { inline: { 1: { name: 'somename' }, 2: { description: 'new description' } } }, scenes: {} })
+
+  wrapper.render()
+
+  expect(wrapper.find('.id').text()).toEqual('12')
+  expect(wrapper.find('.name').text()).toEqual('somename')
+  expect(wrapper.find('.capitalizedName').text()).toEqual('Somename')
+  expect(wrapper.find('.description').text()).toEqual('new description')
+
+  wrapper.unmount()
+
+  // nothing in the store after unmounting
+  expect(store.getState()).toEqual({ kea: {}, scenes: {} })
+})
