c8d83a1265f7d6bc5be1cf33c57321262ecf4177
Marius Andra <marius.andra@gmail.com>
2019-04-27 13:15:09 +0200

    mount cleanup and debug

--- src/kea/mount.js
+++ src/kea/mount.js
@@ -2,19 +2,19 @@ import { attachReducer, detachReducer } from '../store/reducer'
 
 import { getCache } from '../cache'
 
-// we store mounted paths on the store so that they could easily be garbabe collected during SSR
-
 export function mountPaths (logic, plugins) {
   const { mountPathCounter, mountedLogic } = getCache()
 
   Object.keys(logic.connections).forEach(path => {
     mountPathCounter[path] = (mountPathCounter[path] || 0) + 1
     if (mountPathCounter[path] === 1) {
+      // console.log('mounting', path)
       const connectedLogic = logic.connections[path]
       mountedLogic[path] = connectedLogic
 
       // attach reducer to redux if not already attached
       if (connectedLogic.reducer && !connectedLogic.mounted) {
+        // console.log('attached', mountPathCounter)
         attachReducer(connectedLogic.path, connectedLogic.reducer)
         connectedLogic.mounted = true
       }
@@ -30,12 +30,14 @@ export function unmountPaths (logic, plugins, lazy) {
   Object.keys(logic.connections).reverse().forEach(path => {
     mountPathCounter[path] = (mountPathCounter[path] || 0) - 1
     if (mountPathCounter[path] === 0) {
+      // console.log('unmounting', path)
       const connectedLogic = logic.connections[path]
       delete mountedLogic[path]
 
       if (lazy && connectedLogic.reducer && connectedLogic.mounted) {
+        // console.log('detached', mountPathCounter)
         detachReducer(connectedLogic.path, connectedLogic.reducer)
-        connectedLogic.mounted = true
+        connectedLogic.mounted = false
       }
 
       plugins.forEach(p => p.unmounted && p.unmounted(path, connectedLogic))
