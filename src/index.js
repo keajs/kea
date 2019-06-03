751d11d50ce0bc82cdc0a9eb57f12aee9067c38f
Marius Andra <marius.andra@gmail.com>
2019-06-03 09:36:41 +0200

    export open/closeContext

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export { kea, connect } from './kea'
 export { keaReducer, getStore, ATTACH_REDUCER, DETACH_REDUCER } from './store'
 export { activatePlugin } from './plugins'
-export { resetContext, getContext } from './context'
+export { resetContext, openContext, closeContext, getContext } from './context'
 
 export { createAction } from './core/shared/actions'
 export { addConnection } from './core/shared/connect'
