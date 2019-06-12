2025cb5ea4fcee0481018681de4b3857da422977
Marius Andra <marius.andra@gmail.com>
2019-06-12 10:42:03 +0200

    revert to a simpler useProps for now

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,44 +1,53 @@
 import { useMemo, useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { mountPaths, unmountPaths } from '../kea/mount'
 
-export function useProps(logic) {
+export function useProps (logic) {
   useMountedLogic(logic)
 
-  return useMemo(() => {
-    let response = {}
-  
-    for (const key of Object.keys(logic.selectors)) {
-      Object.defineProperty(response, key, {
-        get: () => useSelector(logic.selectors[key])
-      })
-    }
+  let response = {}
 
-    return response
-  }, [])
+  for (const key of Object.keys(logic.selectors)) {
+    response[key] = useSelector(logic.selectors[key])
+  }
+
+  return response
 }
+// export function useProps (logic) {
+//   useMountedLogic(logic)
 
-export function useActions(logic) {
+//   return useMemo(() => {
+//     let response = {}
+
+//     for (const key of Object.keys(logic.selectors)) {
+//       Object.defineProperty(response, key, {
+//         get: () => useSelector(logic.selectors[key])
+//       })
+//     }
+
+//     return response
+//   }, [])
+// }
+
+export function useActions (logic) {
   useMountedLogic(logic)
 
   const dispatch = useDispatch()
 
   return useMemo(() => {
     let response = {}
-  
+
     for (const key of Object.keys(logic.actions)) {
       response[key] = (...args) => dispatch(logic.actions[key](...args))
     }
-    
+
     return response
   }, [dispatch])
 }
 
-export function useMountedLogic(logic) {
-  const firstRender = useRef(true)
-  if (firstRender.current) {
-    firstRender.current = false
-    mountPaths(logic)
+export function useMountedLogic (logic) {
+  const unmount = useRef(undefined)
+  if (!unmount.current) {
+    unmount.current = logic.mount()
   }
-  useEffect(() => () => unmountPaths(logic), [])
+  useEffect(() => unmount.current, [])
 }
