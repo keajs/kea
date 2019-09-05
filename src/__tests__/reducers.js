93794a1f790312dbf2f43a0217157ef4fe0fd540
Marius Andra <marius.andra@gmail.com>
2019-09-05 13:14:19 +0200

    reducers only default test case

--- src/__tests__/reducers.js
+++ src/__tests__/reducers.js
@@ -17,12 +17,13 @@ test('it converts reducer arrays correctly', () => {
       everything: [0, PropTypes.number, { persist: true }, { ACTION: reducerFunction }],
       noProp: [0, { persist: true }, { ACTION: reducerFunction }],
       noOptions: [0, PropTypes.number, { ACTION: reducerFunction }],
-      noPropNoOptions: [0, { ACTION: reducerFunction }]
+      noPropNoOptions: [0, { ACTION: reducerFunction }],
+      onlyDefault: [0]
     })
   })
 
-  expect(Object.keys(logic.reducers).sort()).toEqual(['everything', 'noOptions', 'noProp', 'noPropNoOptions'])
-  expect(Object.keys(logic.defaults).sort()).toEqual(['everything', 'milk', 'noOptions', 'noProp', 'noPropNoOptions'])
+  expect(Object.keys(logic.reducers).sort()).toEqual(['everything', 'noOptions', 'noProp', 'noPropNoOptions', 'onlyDefault'])
+  expect(Object.keys(logic.defaults).sort()).toEqual(['everything', 'milk', 'noOptions', 'noProp', 'noPropNoOptions', 'onlyDefault'])
   expect(Object.keys(logic.propTypes).sort()).toEqual(['everything', 'noOptions'])
   expect(Object.keys(logic.reducerOptions).sort()).toEqual(['everything', 'noProp'])
 
@@ -30,12 +31,14 @@ test('it converts reducer arrays correctly', () => {
   expect(typeof logic.reducers.noProp).toBe('function')
   expect(typeof logic.reducers.noOptions).toBe('function')
   expect(typeof logic.reducers.noPropNoOptions).toBe('function')
+  expect(typeof logic.reducers.onlyDefault).toBe('function')
 
   expect(logic.defaults.everything).toBe(0)
   expect(logic.defaults.milk).toBe('not found')
   expect(logic.defaults.noProp).toBe(0)
   expect(logic.defaults.noOptions).toBe(0)
   expect(logic.defaults.noPropNoOptions).toBe(0)
+  expect(logic.defaults.onlyDefault).toBe(0)
 
   expect(logic.propTypes.everything).toBe(PropTypes.number)
   expect(logic.propTypes.noOptions).toBe(PropTypes.number)
