41153716e2b1be382c45b39973beffd15d2a0f77
Marius Andra <marius.andra@gmail.com>
2019-12-28 22:44:03 +0100

    add props to connect

--- src/kea/convert.js
+++ src/kea/convert.js
@@ -1,7 +1,7 @@
 import { addActions, addConnect, addConstants, addDefaults, addReducers, addSelectors } from '../dsl'
 
 export const convertLogic = input => logic => {
-  input.connect && addConnect(typeof input.connect === 'function' ? input.connect(logic) : input.connect)
+  input.connect && addConnect(typeof input.connect === 'function' ? input.connect(logic.props) : input.connect)
   input.constants && addConstants(input.constants(logic))
   input.actions && addActions(input.actions(logic))
   input.defaults && addDefaults(input.defaults)
