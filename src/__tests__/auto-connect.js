ba0fad79a25c64bf0bb2f51e4ab513e7cd78c315
Marius Andra <marius.andra@gmail.com>
2020-04-09 23:19:58 +0200

    test selectors auto-connect

--- src/__tests__/auto-connect.js
+++ src/__tests__/auto-connect.js
@@ -7,7 +7,7 @@ import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
 
-test('other logic gets connected and mounted automatically when used in e.g. reducers', () => {
+test('other logic gets connected and mounted automatically when used in reducers', () => {
   resetContext({ createStore: true, autoMount: false })
 
   const thirdLogic = kea({
@@ -73,4 +73,93 @@ test('other logic gets connected and mounted automatically when used in e.g. red
   expect(() => { logic.values.name }).toThrow() // eslint-disable-line
   expect(() => { secondLogic.values.secondName }).toThrow() // eslint-disable-line
   expect(() => { thirdLogic.values.secondName }).toThrow() // eslint-disable-line
+})
+
+test('other logic gets connected and mounted automatically when used in selectors', () => {
+  resetContext({ createStore: true, autoMount: false })
+
+  const thirdLogic = kea({
+    actions: () => ({
+      thirdAction: name => ({ name })
+    }),
+    reducers: ({ actions }) => ({
+      thirdName: ['third', {
+        [actions.thirdAction]: (_, { name }) => name
+      }]
+    })
+  })
+
+  const secondLogic = kea({
+    actions: () => ({
+      secondAction: name => ({ name })
+    }),
+    reducers: ({ actions }) => ({
+      secondName: ['second', {
+        [actions.secondAction]: (_, { name }) => name
+      }]
+    }),
+
+    selectors: ({ selectors }) => ({
+      combinedName: [
+        () => [selectors.secondName, thirdLogic.selectors.thirdName],
+        (secondName, thirdName) => `${secondName}.${thirdName}`
+      ]
+    })
+  })
+
+  const logic = kea({
+    actions: () => ({
+      updateName: name => ({ name })
+    }),
+
+    reducers: ({ actions }) => ({
+      name: ['first', {
+        [actions.updateName]: (_, { name }) => name,
+        [secondLogic.actions.secondAction]: (_, { name }) => name
+      }]
+    }),
+
+    selectors: ({ selectors }) => ({
+      combinedName: [
+        () => [selectors.name, secondLogic.selectors.combinedName],
+        (name, combinedName) => `${name}.${combinedName}`
+      ]
+    })
+  })
+
+  const unmount1 = logic.mount()
+
+  expect(logic.values.name).toEqual('first')
+  expect(logic.values.combinedName).toEqual('first.second.third')
+  expect(secondLogic.values.secondName).toEqual('second')
+  expect(secondLogic.values.combinedName).toEqual('second.third')
+  expect(thirdLogic.values.thirdName).toEqual('third')
+
+  unmount1()
+
+  const unmount2 = secondLogic.mount()
+
+  expect(() => { logic.values.name }).toThrow() // eslint-disable-line
+  expect(() => { logic.values.combinedName }).toThrow() // eslint-disable-line
+  expect(secondLogic.values.secondName).toEqual('second')
+  expect(secondLogic.values.combinedName).toEqual('second.third')
+  expect(thirdLogic.values.thirdName).toEqual('third')
+
+  unmount2()
+
+  const unmount3 = thirdLogic.mount()
+
+  expect(() => { logic.values.name }).toThrow() // eslint-disable-line
+  expect(() => { logic.values.combinedName }).toThrow() // eslint-disable-line
+  expect(() => { secondLogic.values.secondName }).toThrow() // eslint-disable-line
+  expect(() => { secondLogic.values.combinedName }).toThrow() // eslint-disable-line
+  expect(thirdLogic.values.thirdName).toEqual('third')
+
+  unmount3()
+
+  expect(() => { logic.values.name }).toThrow() // eslint-disable-line
+  expect(() => { logic.values.combinedName }).toThrow() // eslint-disable-line
+  expect(() => { secondLogic.values.secondName }).toThrow() // eslint-disable-line
+  expect(() => { secondLogic.values.combinedName }).toThrow() // eslint-disable-line
+  expect(() => { thirdLogic.values.thirdName }).toThrow() // eslint-disable-line
 })
