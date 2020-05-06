2761f400b1da502b79073578f3d2d7d79c571d42
Marius Andra <marius.andra@gmail.com>
2020-05-07 00:01:14 +0200

    props is always an object

---
+++ src/__tests__/props.js
@@ -0,0 +1,48 @@
+/* global test, expect, beforeEach */
+import { kea, resetContext } from '../index'
+
+beforeEach(() => {
+  resetContext()
+})
+
+test('always has some props object', () => {
+  let listenerRan = false
+
+  const firstLogic = kea({
+    actions: () => ({
+      updateName: name => ({ name })
+    }),
+    listeners: ({ actions, props }) => ({
+      [actions.updateName]: () => {
+        expect(props).toEqual({})
+        listenerRan = true
+      }
+    })
+  })
+
+  firstLogic.mount()
+  firstLogic.actions.updateName('name')
+
+  expect(listenerRan).toBe(true)
+})
+
+test('always has some props object, part 2', () => {
+  let listenerRan = false
+
+  const firstLogic = kea({
+    actions: () => ({
+      updateName: name => ({ name })
+    }),
+    listeners: ({ actions, props }) => ({
+      [actions.updateName]: () => {
+        expect(props).toEqual({})
+        listenerRan = true
+      }
+    })
+  })
+
+  firstLogic().mount()
+  firstLogic.actions.updateName('name')
+
+  expect(listenerRan).toBe(true)
+})
