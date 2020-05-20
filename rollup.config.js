f25d47bad1b1038d0eeb7e60b04c10e075c7f75a
Marius Andra <marius.andra@gmail.com>
2020-05-20 23:50:35 +0200

    ts config that keeps js working

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,35 @@
 import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
+import pkg from './package.json'
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+
+const external = Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {}))
 
 export default [
   {
     input: './src/index.js',
-    output: { file: 'lib/index.js', format: 'cjs' },
-    external: ['prop-types', 'react', 'redux', 'react-redux', 'reselect'],
-    plugins: [babel(), resolve()]
-  }
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+      },
+    ],
+    external,
+    plugins: [
+      // Allows node_modules resolution
+      resolve({ extensions }),
+      // Allow bundling cjs modules. Rollup doesn't understand cjs
+      commonjs({
+        exclude: 'src/**',
+      }),
+      // Compile TypeScript/JavaScript files
+      babel({ extensions, include: ['src/**/*'] }),
+    ],
+  },
 ]
