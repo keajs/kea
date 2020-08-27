a1406b58f671836202cff51b20c061cef4ecaf8f
Marius Andra <marius.andra@gmail.com>
2020-08-27 22:23:49 +0200

    add fuller test for empty logic

--- test/tsd/index.test-d.ts
+++ test/tsd/index.test-d.ts
@@ -5,7 +5,11 @@ import { expectType } from 'tsd'
 // ... requiring the following comments:
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { kea, MakeLogicType } from '.'
+import { kea, MakeLogicType, BuiltLogic, PathType, Selector } from '.'
+
+/*
+ * 1. Setup Test MakeLogicType<Values, Actions, Props>
+ */
 
 interface DashboardValues {
   id: number
@@ -27,11 +31,111 @@ type MyLogicType = MakeLogicType<DashboardValues, DashboardActions, DashboardPro
 
 const logic = kea<MyLogicType>({})
 
-expectType<(name: string) => { type: string; payload: { name: string } }>(logic.actionCreators.setName)
-expectType<(name: string) => void>(logic.actions.setName)
+/*
+ * 2. Test MakeLogicType<Values, Actions, Props>
+ *    - Test overridden fields
+ */
 
+expectType<{
+  setName: (name: string) => { type: string; payload: { name: string } }
+  setUsername: (username: string) => { type: string; payload: { username: string } }
+}>(logic.actionCreators)
+
+expectType<Record<string, string>>(logic.actionKeys)
+
+expectType<{
+  setName: string
+  setUsername: string
+}>(logic.actionTypes)
+
+expectType<{
+  setName: (name: string) => void
+  setUsername: (username: string) => void
+}>(logic.actions)
+
+expectType<DashboardValues>(logic.defaults)
 expectType<DashboardProps>(logic.props)
+expectType<DashboardValues>(logic.values)
+expectType<(state: DashboardValues, action: () => any, fullState: any) => DashboardValues>(logic.reducer)
 
-expectType<number>(logic.values.id)
-expectType<(state: any, props: DashboardProps) => number>(logic.selectors.id)
-expectType<(state: number, action: () => any, fullState: any) => number>(logic.reducers.id)
+expectType<{
+  id: (state: number, action: () => any, fullState: any) => number
+  created_at: (state: string, action: () => any, fullState: any) => string
+  name: (state: string, action: () => any, fullState: any) => string
+  pinned: (state: boolean, action: () => any, fullState: any) => boolean
+}>(logic.reducers)
+
+expectType<(state: any, props: DashboardProps) => DashboardValues>(logic.selector)
+
+expectType<{
+  id: (state: any, props: DashboardProps) => number
+  created_at: (state: any, props: DashboardProps) => string
+  name: (state: any, props: DashboardProps) => string
+  pinned: (state: any, props: DashboardProps) => boolean
+}>(logic.selectors)
+
+expectType<{
+  id: (...args: any) => number
+  created_at: (...args: any) => string
+  name: (...args: any) => string
+  pinned: (...args: any) => boolean
+}>(logic.__keaTypeGenInternalSelectorTypes)
+
+/*
+ * 3. Test MakeLogicType<Values, Actions, Props>
+ *    - Test default fields
+ */
+
+expectType<any>(logic.key)
+expectType<Record<string, any>>(logic.cache)
+expectType<{ [pathString: string]: BuiltLogic }>(logic.connections)
+expectType<Record<string, string>>(logic.constants)
+expectType<Record<string, any>>(logic.listeners)
+expectType<PathType>(logic.path)
+expectType<string>(logic.pathString)
+expectType<Record<string, any>>(logic.propTypes)
+expectType<Record<string, any>>(logic.reducerOptions)
+expectType<{
+  beforeMount?: (() => void) | undefined
+  afterMount?: (() => void) | undefined
+  beforeUnmount?: (() => void) | undefined
+  afterUnmount?: (() => void) | undefined
+}>(logic.events)
+expectType<Record<string, (...args: any) => { type: string; payload: any }>>(logic.__keaTypeGenInternalReducerActions)
+
+/*
+ * 4. Test Empty Logic
+ */
+
+const logic2 = kea({})
+
+expectType<any>(logic2.key)
+expectType<Record<string, any>>(logic2.cache)
+expectType<{ [pathString: string]: BuiltLogic }>(logic2.connections)
+expectType<Record<string, string>>(logic2.constants)
+expectType<Record<string, any>>(logic2.listeners)
+expectType<PathType>(logic2.path)
+expectType<string>(logic2.pathString)
+expectType<Record<string, any>>(logic2.propTypes)
+expectType<Record<string, any>>(logic2.reducerOptions)
+expectType<{
+  beforeMount?: (() => void) | undefined
+  afterMount?: (() => void) | undefined
+  beforeUnmount?: (() => void) | undefined
+  afterUnmount?: (() => void) | undefined
+}>(logic2.events)
+expectType<Record<string, (...args: any) => { type: string; payload: any }>>(logic2.__keaTypeGenInternalReducerActions)
+
+// new compared to test 3
+expectType<any>(logic2.actionCreators)
+expectType<Record<string, string>>(logic2.actionKeys)
+expectType<Record<string, string>>(logic2.actionTypes)
+expectType<any>(logic2.actions)
+expectType<Record<string, any>>(logic2.defaults)
+expectType<any>(logic2.props)
+expectType<Record<string, any>>(logic2.values)
+expectType<any>(logic2.reducer)
+expectType<any>(logic2.reducers)
+expectType<Selector | undefined>(logic2.selector)
+expectType<Record<string, Selector>>(logic2.selectors)
+expectType<Record<string, (...args: any) => any>>(logic2.__keaTypeGenInternalSelectorTypes)
