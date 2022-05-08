f6b8cb51698e43514170897e46822cfd9dda733c
Marius Andra <marius.andra@gmail.com>
2022-05-08 20:42:26 +0200

    better way to do props

--- src/core/props.ts
+++ src/core/props.ts
@@ -1,17 +1,12 @@
 import { Logic, LogicBuilder } from '../types'
+import { shallowCompare } from '../utils'
 
 export function props<L extends Logic = Logic>(input: L['props']): LogicBuilder<L> {
   return (logic) => {
-    let change = false
-    for (const [key, value] of Object.entries(input)) {
-      if (logic.props[key] !== value) {
-        change = true // don't modify the reference if resulting props are identical
-        break
-      }
-    }
-    if (change) {
-      // set the props that haven't been passed in to the defaults
-      logic.props = { ...input, ...logic.props }
+    // set the props that haven't been initialized on build
+    const newProps = { ...input, ...logic.props }
+    if (!shallowCompare(logic.props, newProps)) {
+      logic.props = newProps
     }
   }
 }
